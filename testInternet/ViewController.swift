e34e2dadd99c0d7147773cdd77232947c5e81f10
Khac vu Nguyen <>
2019-04-14 09:51:08 +0700

    [demo]

---
+++ testInternet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testInternet
+//
+//  Created by Mac on 2/7/19.
+//  Copyright © 2019 anh vu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
